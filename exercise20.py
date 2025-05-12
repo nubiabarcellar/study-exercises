fd684345ff600b91182503c7300f1aa846251878
Núbia Barcellar <nubiabarcellar@gmail.com>
2025-05-12 17:37:58 -0300

    feat: exercise20 sum of numbers

---
+++ exercise20.py
@@ -0,0 +1,10 @@
+#Exercício 1: Soma de dois números
+#Escreva um programa que receba dois números do usuário e exiba a soma deles.
+
+
+print("Digite um número: ")
+num1 = int(input())
+print("Digite outro número: ")
+num2 = int(input())
+sum = num1 + num2
+print("A soma dos números ", num1, " e ", num2, " é: ", sum)
