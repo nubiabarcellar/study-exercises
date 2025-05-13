8fbe1a1b0cfc9990b8100d7e03394ef5b2b862c3
Núbia Barcellar <nubiabarcellar@gmail.com>
2025-05-12 22:19:14 -0300

    refactor: exercise21

--- exercise21.py
+++ exercise21.py
@@ -1,10 +1,15 @@
-#Exercício 2: Verificar se um número é par ou ímpar
-#Escreva um programa que receba um número do usuário e informe se ele é par ou ímpar.
+# Exercício 2: Verificar se um número é par ou ímpar
+# Escreva um programa que receba um número do usuário e informe se ele é par ou ímpar.
 
-print("Digite um número e descubra se ele é par ou impar:")
-num = int(input())
+class ChecksIfNumberIsEvenOrOdd:
+    def __init__(self, num: int):
+        self.num = num
 
-if(num % 2 == 0):
-    print("O número é par!")
-else:
-    print("O número é impar!")
+    def check_if_number_is_even_or_odd(self) -> str:
+        return "par" if (self.num % 2 == 0) else "impar"
+
+num_input = int(input("Digite um número e descubra se ele é par ou impar:"))
+
+number_checked = ChecksIfNumberIsEvenOrOdd(num_input)
+
+print(f"O número ", num_input, " é ", number_checked.check_if_number_is_even_or_odd())
