494f4e9ad5b09546012436b47d69a6d3784de0a6
Núbia Barcellar <nubiabarcellar@gmail.com>
2025-05-12 21:46:53 -0300

    refactor: exercise20

--- exercise20.py
+++ exercise20.py
@@ -1,10 +1,21 @@
-#Exercício 1: Soma de dois números
-#Escreva um programa que receba dois números do usuário e exiba a soma deles.
+# Exercício 1: Soma de dois números
+# Escreva um programa que receba dois números do usuário e exiba a soma deles.
+
+class Sum_Numbers:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def add_numbers(self) -> int:
+        return self.num1 + self.num2
 
 
 print("Digite um número: ")
 num1 = int(input())
 print("Digite outro número: ")
 num2 = int(input())
-sum = num1 + num2
-print("A soma dos números ", num1, " e ", num2, " é: ", sum)
+
+sum_result = Sum_Numbers(num1, num2)
+
+print("A soma dos números ", num1, " e ",
+      num2, " é: ", sum_result.add_numbers())
